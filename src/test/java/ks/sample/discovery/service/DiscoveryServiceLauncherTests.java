e1174092125564e34c9ad13bb4a26d272559b8b7
Kumar Saurabh <kumarsaurabh2006@gmail.com>
2018-10-16 23:29:56 +0530

    Adding Dicovery Service

---
+++ src/test/java/ks/sample/discovery/service/DiscoveryServiceLauncherTests.java
@@ -0,0 +1,16 @@
+package ks.sample.discovery.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiscoveryServiceLauncherTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
